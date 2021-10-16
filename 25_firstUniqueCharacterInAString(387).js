7c8032587e9c000182c04d0bac13680b2b248c10
Harshita <harshitasharma1600@gmail.com>
2021-10-16 22:17:05 +0530

    File added

---
+++ 25_firstUniqueCharacterInAString(387).js
@@ -0,0 +1,26 @@
+let frequencies = {};
+    let result = -1;
+    
+    for(let char of s)
+        {
+            if(frequencies[char] === undefined)
+                {
+                    frequencies[char] = 1;
+                }
+            else
+                {
+                    frequencies[char]++;
+                }
+        }
+    
+    for(let i = 0;  i < s.length; i++)
+        {
+            let char = s.charAt(i);
+            
+            if(frequencies[char] === 1)
+                {
+                    return i;
+                }
+        }
+    
+    return result;
